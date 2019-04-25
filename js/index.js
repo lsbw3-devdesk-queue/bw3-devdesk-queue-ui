97afc3aa9eba2746b70fb8b0157f890deebf8b92
Ash <ashley.cruz.crespo@gmail.com>
2019-04-24 21:19:27 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 window.addEventListener('resize', (event) => {
-    event.target.style.textTransform = "uppercase";
     console.log(`You're resizing the window`);
 })
-
-const campaingImg = document.querySelector('.hidden');
 
 const mainHeader = document.querySelector('h1');
 mainHeader.addEventListener('click', (event) => {
@@ -15,70 +12,13 @@ mainHeader.addEventListener('click', (event) => {
     setTimeout(function(){event.target.style.textShadow = ""}, 3000);
 }) 
 
-const navLinks = document.querySelector('.nav');
-navLinks.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === "A") {
-    event.target.style.textShadow = "2px 2px 2px lightseagreen";
-    setTimeout(function(){event.target.style.textShadow = "none"}, 300);
-    }
-    event.preventDefault();
+const Btn1 = document.querySelector('#btn-1');
+    Btn1.addEventListener('click', (event) => {
+    alert(`Are you sure you don't want help?`);
+    location.href='about.html';
 })
 
-
-const compimg = document.querySelector('.compimg');
-console.log(compimg);
-compimg.addEventListener('dblclick', (event) => {
-    event.target.setAttribute ("src", "img/cetteup-72915-unsplash.jpg");
-    event.stopPropagation();
-})
-
-compimg.addEventListener('wheel', (event) => {
-    event.target.setAttribute ("src", "img/cetteup-72915-unsplash.jpg");
-})
-
-const Btn1 = document.querySelector('.btn-1');
-Btn1.addEventListener('mouseenter', (event) => {
-    event.target.alert(`Are you sure you don't want help?`)
-})
-
-const Btn2 = document.querySelector('.btn-2');
-    Btn2.addEventListener('mouseenter', (event) => {
-    event.target.style.transform = "scale(2)";
-    setTimeout(function(){event.target.style.transform = ""}, 3000);
-})
-
-const home = document.querySelector('.home');
-
-home.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === "P"){
-    event.target.style.color = "white";
-    event.target.style.background = "black";
-    setTimeout(function(){event.target.style.color = ""}, 3000);
-    setTimeout(function(){event.target.style.background = ""}, 2000);
-    }
-})
-
-home.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === "H2" || event.target.tagName === "H4"){
-    event.target.style.color = "white";
-    event.target.style.background = "black";
-    setTimeout(function(){event.target.style.color = ""}, 3000);
-    setTimeout(function(){event.target.style.background = ""}, 2000);
-    }
-})
-
-window.addEventListener('dblclick', (event) => {
-    event.target.style.background = "purple";
-    setTimeout(function(){event.target.style.background = ""}, 5000);
-})
-
-const mainContain = document.querySelector('.home')
-
-mainContain.addEventListener('dblclick', (event) => {
-    event.stopPropagation();
-})
-
-const topBar = document.querySelector('.main-navigation');
-topBar.addEventListener('dblclick', (event) => {
-    event.stopPropagation();
+const Btn2 = document.querySelector('#btn-2');
+    Btn2.addEventListener('click', (event) => {
+    alert(`The FAQ Page is Under Construction`);
 })
